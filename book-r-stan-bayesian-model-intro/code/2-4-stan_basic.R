81e653b94d2eec46f1ee70714787e683a5f68a89
SatsukiNagae <n.sakki55@gmail.com>
2020-03-15 13:20:49 +0900

    code

---
+++ book-r-stan-bayesian-model-intro/code/2-4-stan_basic.R
@@ -0,0 +1,106 @@
+# パッケージの読み込み
+library(rstan)
+
+# 計算の高速化
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+
+
+# データの読み込み ----------------------------------------------------------------
+
+# 分析対象のデータ
+file_beer_sales_1 <- read.csv("2-4-1-beer-sales-1.csv")
+
+# データの確認
+head(file_beer_sales_1, n = 3)
+
+
+# Stanに渡すためにデータを整形する ------------------------------------------------------
+
+# サンプルサイズ
+sample_size <- nrow(file_beer_sales_1)
+sample_size
+
+# listにまとめる
+data_list <- list(sales = file_beer_sales_1$sales, N = sample_size)
+data_list
+
+
+# MCMCによるサンプリングの実施 -----------------------------------------------------------------
+
+# 乱数の生成
+mcmc_result <- stan(
+  file = "2-4-1-calc-mean-variance.stan", # stanファイル
+  data = data_list,                       # 対象データ
+  seed = 1,                               # 乱数の種
+  chains = 4,                             # チェーン数
+  iter = 2000,                            # 乱数生成の繰り返し数
+  warmup = 1000,                          # バーンイン期間
+  thin = 1                                # 間引き数(1なら間引き無し) 
+)
+
+# 結果の表示
+print(
+  mcmc_result,                   # MCMCサンプリングの結果
+  probs = c(0.025, 0.5, 0.975)   # 中央値と95%信用区間を出力
+)
+
+traceplot(mcmc_result)
+
+traceplot(mcmc_result, inc_warmup=T)
+
+
+mcmc_sample <- rstan::extract(mcmc_result, permuted = FALSE)
+class(mcmc_sample)
+dim(mcmc_sample)
+dimnames(mcmc_sample)
+mcmc_sample[1, 'chain:1', 'mu']
+mcmc_sample[, 'chain:1', 'mu']
+length(mcmc_sample[, 'chain:1', 'mu'])
+length(mcmc_sample[, , 'mu'])
+dim(mcmc_sample[, , 'mu'])
+class(mcmc_sample[, , 'mu'])
+
+mu_mcmc_vec <- as.vector(mcmc_sample[, , 'mu'])
+median(mu_mcmc_vec)
+mean(mu_mcmc_vec)
+quantile(mu_mcmc_vec, probs=c(0.025, 0.975))
+
+print(
+  mcmc_result,                   # MCMCサンプリングの結果
+  probs = c(0.025, 0.5, 0.975)   # 中央値と95%信用区間を出力
+)
+
+library(ggfortify)
+autoplot(ts(mcmc_sample[, , 'mu']),
+         facets = F,
+         ylab = 'mu',
+         main = 'trace plot')
+
+mu_df <- data.frame(
+  mu_mcmc_sample = mu_mcmc_vec
+)
+
+ggplot(data = mu_df, mapping = aes(x = mu_mcmc_sample)) + geom_density(size=1.5)
+
+
+library(bayesplot)
+
+mcmc_hist(mcmc_sample, pars = c('mu', 'sigma'))
+mcmc_dens(mcmc_sample, pars = c('mu', 'sigma'))
+mcmc_trace(mcmc_sample, pars = c('mu', 'sigma'))
+mcmc_combo(mcmc_sample, pars = c('mu', 'sigma'))
+
+mcmc_intervals(
+  mcmc_sample, pars = c('mu', 'sigma'),
+  prob = 0.8,
+  prob_outer = 0.95
+)
+
+mcmc_areas(
+  mcmc_sample, pars = c('mu', 'sigma'),
+  prob = 0.6,
+  prob_outer = 0.99
+)
+
+mcmc_acf_bar(mcmc_sample, pars = c('mu', 'sigma'))
